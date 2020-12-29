c8a358e03f3218fbe7aa396063cc3052ef368d9f
swapnil.kamble <swapnil.kamble@us.imshealth.com>
2020-12-29 10:05:00 +0530

    Linkedin Learning code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FaStar } from 'react-icons/fa';
+
+const starRatingArray = length => [...Array(length)];
+
+function Star({ selected = false, onSelected }) {
+  return <FaStar
+    color={selected ? 'red' : 'gray'}
+    onClick={onSelected} />
+}
+
+function StarRating() {
+  const [starSelected, setStarSelected] = useState(0);
+  return starRatingArray(5).map((n, i) => (
+    <Star key={i}
+      selected={starSelected > i}
+      onSelected={() => setStarSelected(i + 1)}
+    />
+  ));
+}
+
+function ExampleFetch() {
+  const [userData, setUserdata] = useState([]);
+  useEffect(() => {
+    fetch(`https://api.github.com/users`)
+      .then(res => res.json())
+      .then(setUserdata)
+      .catch(err => console.log(err))
+  },[])
+
+  if (userData) {
+    return <ul>{
+      userData.map((n, i) => (
+        <li>{n.login}</li>
+      ))
+    }</ul>
+  }
+}
+
+function App() {
+  return (<>
+    <StarRating />
+    <ExampleFetch />
+  </>)
+
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
